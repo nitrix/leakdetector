c48db23620596c0799a2b5542dff72e59f89564b
Alex Belanger <nitrix@nitrix.me>
2025-06-09 16:33:16 -0400

    Small improvements towards a hashtable.

--- src/leakdetector.c
+++ src/leakdetector.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #if !defined(__has_builtin)
 #define __has_builtin(x) 0
@@ -13,74 +14,97 @@
 #define LIKELY(x)   __builtin_expect(!!(x), 1)
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
-static void **pointers = NULL;
-static size_t pointers_count = 0;
-static size_t pointers_capacity = 0;
-static char **locations = NULL;
-pthread_mutex_t pointers_mutex = PTHREAD_MUTEX_INITIALIZER;
+struct entry {
+    void *ptr;
+    size_t size;
+    const char *file;
+    int line;
+    struct entry *next;
+};
 
-static void _trap(void) {
+static struct entry **buckets = NULL;
+static size_t buckets_count = 0;
+static size_t entries_count = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void trap(void) {
     __asm__ volatile ("int $0x03");
 }
 
 void leakdetector_check(void) {
-    if (pointers_count) {
-        fprintf(stderr, "Memory leak detected: %zu alive allocations\n", pointers_count);
+    if (entries_count > 0) {
+        fprintf(stderr, "Memory leak detected! (%zu still alive)\n", entries_count);
 
-        for (size_t i = 0; i < pointers_count; i++) {
-            fprintf(stderr, "-> %p allocated at %s\n", pointers[i], locations[i]);
+        for (size_t i = 0; i < buckets_count; i++) {
+            const struct entry *entry = buckets[i];
+            while (entry) {
+                fprintf(stderr, "\t-> %p allocated at %s:%d (%zu bytes)\n", entry->ptr, entry->file, entry->line, entry->size);
+                entry = entry->next;
+            }
         }
 
-        _trap();
+        trap();
     }
 }
 
-static void _setup_atexit_check(void) {
-    static bool initialized = false;
-    if (!initialized) {
+static void setup_atexit_check(void) {
+    static bool setup = false;
+    if (!setup) {
         atexit(leakdetector_check);
-        initialized = true;
+
+        size_t initial_buckets_count = 1024;
+        buckets = malloc(initial_buckets_count * sizeof *buckets);
+
+        if (UNLIKELY(!buckets)) {
+            fprintf(stderr, "Failed to allocate memory for leakdetector buckets\n");
+            exit(EXIT_FAILURE);
+        }
+
+        buckets_count = initial_buckets_count;
+
+        for (size_t i = 0; i < buckets_count; i++) {
+            buckets[i] = NULL;
+        }
+
+        setup = true;
     }
+}
+
+static inline uintptr_t hash_ptr(uintptr_t ptr) {
+    ptr ^= (ptr >> 33);
+    ptr *= 0xff51afd7ed558ccdULL;
+    ptr ^= (ptr >> 33);
+    ptr *= 0xc4ceb9fe1a85ec53ULL;
+    ptr ^= (ptr >> 33);
+    return ptr;
 }
 
 void leakdetector_add(void *ptr, size_t size, const char *file, int line) {
-    _setup_atexit_check();
+    pthread_mutex_lock(&mutex);
 
-    pthread_mutex_lock(&pointers_mutex);
+    setup_atexit_check();
 
-    if (pointers_count == pointers_capacity) {
-        pointers_capacity = pointers_capacity ? pointers_capacity * 2 : 16;
-        void **new_pointers = realloc(pointers, pointers_capacity * sizeof *new_pointers);
-        if (UNLIKELY(!new_pointers)) {
-            fprintf(stderr, "Failed to reallocate memory\n");
-            exit(EXIT_FAILURE);
-        }
+    uintptr_t hash = hash_ptr((uintptr_t)ptr);
+    size_t index = hash % buckets_count;
 
-        char **new_locations = realloc(locations, pointers_capacity * sizeof *new_locations);
-        if (UNLIKELY(!new_locations)) {
-            fprintf(stderr, "Failed to reallocate memory\n");
-            exit(EXIT_FAILURE);
-        }
-
-        pointers = new_pointers;
-        locations = new_locations;
-    }
-
-    pointers[pointers_count] = ptr;
-
-    size_t length = snprintf(NULL, 0, "%s:%d (%zu bytes)", file, line, size);
-    char *str = malloc(length + 1);
-    if (UNLIKELY(!str)) {
-        fprintf(stderr, "Failed to allocate memory\n");
+    struct entry *new_entry = malloc(sizeof *new_entry);
+    if (UNLIKELY(!new_entry)) {
+        fprintf(stderr, "Failed to allocate memory for leakdetector new entry\n");
         exit(EXIT_FAILURE);
     }
 
-    snprintf(str, length + 1, "%s:%d (%zu bytes)", file, line, size);
+    new_entry->ptr = ptr;
+    new_entry->size = size;
+    new_entry->file = file;
+    new_entry->line = line;
+    new_entry->next = buckets[index];
+    buckets[index] = new_entry;
+    entries_count++;
 
-    locations[pointers_count] = str;
+    // TODO: Resize the hashmap if necessary.
 
-    pointers_count++;
-    pthread_mutex_unlock(&pointers_mutex);
+    pthread_mutex_unlock(&mutex);
 }
 
 void leakdetector_remove(void *ptr) {
@@ -88,24 +112,41 @@ void leakdetector_remove(void *ptr) {
         return;
     }
 
-    pthread_mutex_lock(&pointers_mutex);
+    pthread_mutex_lock(&mutex);
+
+    uintptr_t hash = hash_ptr((uintptr_t)ptr);
+    size_t index = hash % buckets_count;
+
+    struct entry *current = buckets[index];
+    struct entry *prev = NULL;
 
     bool found = false;
-    for (size_t i = 0; i < pointers_count; i++) {
-        if (pointers[i] == ptr) {
-            pointers[i] = pointers[pointers_count - 1];
-            free(locations[i]);
-            locations[i] = locations[pointers_count - 1];
-            pointers_count--;
+
+    while (current) {
+        if (current->ptr == ptr) {
+            if (prev) {
+                prev->next = current->next;
+            } else {
+                buckets[index] = current->next;
+            }
+
+            free(current);
+            entries_count--;
+
             found = true;
             break;
         }
+
+        prev = current;
+        current = current->next;
     }
 
     if (!found) {
         fprintf(stderr, "Freeing unallocated memory: %p\n", ptr);
-        _trap();
+        trap();
     }
 
-    pthread_mutex_unlock(&pointers_mutex);
+    // TODO: Resize the hashmap if necessary.
+
+    pthread_mutex_unlock(&mutex);
 }
